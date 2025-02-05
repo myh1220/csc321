36d87630a98d51c0ea9aea0be35fc755f007386b
ym <myh0435@gmail.com>
2025-02-05 02:29:05 +0000

    menu version 1

--- ymin4.java
+++ ymin4.java
@@ -9,15 +9,15 @@ public class ymin4
 	{
 		int x = 0;
 
-		System.out.println("Choose an option");
-		System.out.println("Option 1");
-		System.out.println("Option 2");
-		System.out.println("Option 3");
-		System.out.println("Option 4");
+		System.out.println("Pick your favorite season.");
+		System.out.println("Option 1: Spring");
+		System.out.println("Option 2: Summer");
+		System.out.println("Option 3: Fall");
+		System.out.println("Option 4: Winter");
 
 		Scanner userinput = new Scanner(System.in);
 		x = userinput.nextInt();
 
-		System.out.println("you chose option " +x);
+		System.out.println("You chose option " +x+ " as your favorite season.");
 	}
 }
